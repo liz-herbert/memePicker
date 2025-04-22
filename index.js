aaf44ea2d6f0c29e5f991f1b5f09f84b4dfc94d6
Liz H. <137332408+liz-herbert@users.noreply.github.com>
2025-04-21 20:16:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -171,10 +171,14 @@ const catsData = [
     },
 ]
 
+//take control of div with emotion-radio id
+const emotionRadios = document.getElementById("emotion-radios")
+
 function getEmotionsArray(cats){
 
     const emotionsArray = []
     
+    //iterate through the cats object array
     for (let cat of cats){
         //push each emotion to the emotionsArray
         for (let emotion of cat.emotionTags){
@@ -182,8 +186,19 @@ function getEmotionsArray(cats){
         }
         
     }
-    console.log(emotionsArray)
+    return emotionsArray
 }
 
-getEmotionsArray(catsData)
+function renderEmotionsRadios(cats){
+    let radioItems = ``
+    const emotions = getEmotionsArray(cats)
+    
+    //iterate through each emotion to add to radioItems
+    for (let emotion of emotions) {
+        radioItems += `<p>${emotion}</p>`
+    }
+    //update the HTML to reflect the radioItems
+    emotionRadios.innerHTML = radioItems
+}
 
+renderEmotionsRadios(catsData)
